5b78605635fb780fe7911dcc8369fa206fc4302c
Federico Di Domenicantonio <fededido91@hotmail.it>
2016-11-16 15:13:43 +0100

    Connects to aws using custom session

--- teacup-storm.py
+++ teacup-storm.py
@@ -4,21 +4,32 @@ This version simply starts and stops machines on AWS.
 import boto3
 import yaml
 
-def main():
-    # Read configuration.
+
+def createSession():
+    #Read configuration
     stream = open("config.yaml")
     conf = yaml.load(stream)
     ACCESS_KEY = conf["id"]
     SECRET_KEY = conf["key"]
     REGION = conf["region"]
-
-    # Print list of s3 buckets.
-    s3 = boto3.resource(
-        's3',
+    
+    session = boto3.session.Session(
         aws_access_key_id=ACCESS_KEY,
         aws_secret_access_key=SECRET_KEY,
         region_name=REGION
     )
+    return session
+
+def main():
+    # Print list of s3 buckets.
+    '''s3 = boto3.resource(
+        's3',
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        region_name=REGION
+    )'''
+    session = createSession()
+    s3 = session.resource('s3')
     for bucket in s3.buckets.all():
         print(bucket.name)
 
