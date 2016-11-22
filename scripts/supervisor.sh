fd5b23719c305f0b84caedd124e3dcce595996b8
Federico Di Domenicantonio <fededido91@hotmail.it>
2016-11-22 18:43:54 +0100

    Adds skeleton of supervisor implementation
    Zookeper ought to be run in supervisor mode, this commit installs
    the required packages and performs basic config.  It is still not
    working, as zk port cannot be reached.

--- scripts/supervisor.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This script installs the supervisor supervisord, found at www.supervisord.org
-easy_install supervisor
-echo_supervisord_conf > /etc/supervisord.conf
-echo "[program:zookeeper]\ncommand=zkServer.sh start-foreground\nautorestart=true\nstopsignal=KILL\n" > /etc/supervisord.conf
-
--- scripts/zookeeper.sh
+++ scripts/zookeeper.sh
@@ -7,6 +7,30 @@ rm zookeeper-3.4.6.tar.gz
 # Configures ZooKeeper.
 mkdir /var/zookeeper
 cd zookeeper-3.4.6
-echo "tickTime=2000\ndataDir=/var/zookeeper\nclientPort=2181" > ./conf/zoo.cfg
-# Starts ZooKeeper.
-./bin/zkServer.sh start
+echo "tickTime=2000" >  ./conf/zoo.cfg
+echo "dataDir=/var/zookeeper" >> ./conf/zoo.cfg
+echo "clientPort=2181" >> ./conf/zoo.cfg
+# This script installs the supervisor supervisord, found at www.supervisord.org
+easy_install supervisor
+echo "[unix_http_server]" > /etc/supervisord.conf
+echo "file=/tmp/supervisor.sock" >> /etc/supervisord.conf
+echo "[supervisord]" >> /etc/supervisord.conf
+echo "logfile=/tmp/supervisord.log" >> /etc/supervisord.conf
+echo "logfile_maxbytes=50MB" >> /etc/supervisord.conf
+echo "logfile_backups=10" >> /etc/supervisord.conf
+echo "loglevel=info" >> /etc/supervisord.conf
+echo "pidfile=/tmp/supervisord.pid" >> /etc/supervisord.conf
+echo "nodaemon=false" >> /etc/supervisord.conf
+echo "minfds=1024" >> /etc/supervisord.conf
+echo "minprocs=200" >> /etc/supervisord.conf
+echo "[rpcinterface:supervisor]" >> /etc/supervisord.conf
+echo "supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface" >> /etc/supervisord.conf
+echo "[supervisorctl]" >> /etc/supervisord.conf
+echo "serverurl=unix:///tmp/supervisor.sock" >> /etc/supervisord.conf
+echo "[program:zookeeper]" >> /etc/supervisord.conf
+echo "command=/opt/zookeeper-3.4.6/bin/zkServer.sh start-foreground" >> /etc/supervisord.conf
+echo "autorestart=true" >> /etc/supervisord.conf
+echo "stopsignal=KILL" >> /etc/supervisord.conf
+
+# Starts ZooKeeper in supervisor mode.
+supervisord -c /etc/supervisord.conf
