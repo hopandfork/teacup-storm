99d9551a3e6fdd9993dcd6ab58ea81a6ad6ad0ba
Gabriele Russo Russo <gabri.russo17@gmail.com>
2016-12-09 17:02:23 +0100

    Extends skeleton for setup-security-groups.py

--- setup-security-groups.py
+++ setup-security-groups.py
@@ -11,6 +11,10 @@ from configuration import Configuration
 # Global configuration object
 config=None
 
+# Needed security groups
+group_names = [ 'teacup-storm-ssh', 'teacup-storm-ui', 'teacup-storm-zk', 'teacup-storm-nimbus', 'teacup-storm-sv' ]
+security_groups = { name:None for name in group_names}
+
         
 ''' Creates a session using user-provided custom credentials. '''
 def create_session():
@@ -22,16 +26,55 @@ def create_session():
     )
     return session
 
-def create_security_groups(session):
-    # TODO
+def create_security_group(vpc, name, description=''):
+    try:
+        return vpc.create_security_group(GroupName=name, Description=description)    
+    except boto3.exceptions.botocore.client.ClientError as e:
+        print("[ERROR] Failed to create security group '{}' ({})"
+                .format(_name, e.response['Error']['Code']))
+        exit(2)
+
+def create_zookeeper_sg(vpc):
+    sg = create_security_group(vpc, 'teacup-storm-zk', 'Opens ports for ZK')
+    # TODO configure group
+
+def print_configuration():
     pass
+    # TODO
+
+def get_group_by_name (vpc, name):
+    sg = None
+
+    filters=[{"Name":"group-name", "Values": ["{}".format(name)]}]
+    for i in vpc.security_groups.filter(filters):
+        sg = i
+
+    return sg
 
 def main():
     global config
+
     config = Configuration()
     session = create_session()
+    vpc = session.resource('ec2').Vpc(config.default_vpc)
 
-    create_security_groups(session)
+    # Searches for already existing groups (if any)
+    for name in security_groups.keys():
+        security_groups[name] = get_group_by_name(vpc, name)
+        if security_groups[name] != None:
+            print("[WARNING] Security group '{}' already exists (please, rename "
+                   "it if you manually created it for a different purpose and "
+                   "re-run this tool)".format(name))
+
+    # Creates needed groups
+    if security_groups['teacup-storm-zk'] == None:
+        security_groups['teacup-storm-zk'] = create_zookeeper_sg(vpc)
+
+
+    # TODO other groups
+
+    # Prints configuration to paste into YAML file
+    print_configuration()
     
 if __name__ == "__main__":
     main()
