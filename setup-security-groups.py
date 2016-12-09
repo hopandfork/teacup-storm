e0023a891d2c3f5bb02ae463b9192af8564a386f
Gabriele Russo Russo <gabri.russo17@gmail.com>
2016-12-09 18:02:23 +0100

    Completes setup-security-groups.py

--- setup-security-groups.py
+++ setup-security-groups.py
@@ -36,17 +36,51 @@ def create_security_group(vpc, name, description=''):
 
 def create_zookeeper_sg(vpc):
     sg = create_security_group(vpc, 'teacup-storm-zk', 'Opens ports for ZK')
-    # TODO configure group
+    sg.authorize_ingress(IpProtocol="tcp",CidrIp="0.0.0.0/0",FromPort=2181,ToPort=2181)
+    sg.authorize_ingress(IpProtocol="udp",CidrIp="0.0.0.0/0",FromPort=2181,ToPort=2181)
+
+def create_nimbus_sg(vpc):
+    sg = create_security_group(vpc, 'teacup-storm-nimbus', 'Opens ports for Nimbus')
+    sg.authorize_ingress(IpProtocol="tcp",CidrIp="0.0.0.0/0",FromPort=6627,ToPort=6627)
+    sg.authorize_ingress(IpProtocol="udp",CidrIp="0.0.0.0/0",FromPort=6627,ToPort=6627)
+
+def create_supervisor_sg(vpc):
+    sg = create_security_group(vpc, 'teacup-storm-sv', 'Opens ports for Supervisor')
+    sg.authorize_ingress(IpProtocol="tcp",CidrIp="0.0.0.0/0",FromPort=6700,ToPort=6703)
+    sg.authorize_ingress(IpProtocol="udp",CidrIp="0.0.0.0/0",FromPort=6700,ToPort=6703)
+
+def create_ui_sg(vpc):
+    sg = create_security_group(vpc, 'teacup-storm-ui', 'Opens ports for UI')
+    sg.authorize_ingress(IpProtocol="tcp",CidrIp="0.0.0.0/0",FromPort=8080,ToPort=8080)
+
+def create_ssh_sg(vpc):
+    sg = create_security_group(vpc, 'teacup-storm-ssh', 'Opens ports for SSH')
+    sg.authorize_ingress(IpProtocol="tcp",CidrIp="0.0.0.0/0",FromPort=22,ToPort=22)
 
 def print_configuration():
-    pass
-    # TODO
+    """ Prints lines to be pasted into config.yaml. """
+    print("Add following lines to config.yaml")
+    print(30*'-')
+
+    _ssh = security_groups['teacup-storm-ssh'].id
+    _default = config.default_vpc_security_group
+    _ui = security_groups['teacup-storm-ui'].id
+    _sv = security_groups['teacup-storm-sv'].id
+    _nimbus = security_groups['teacup-storm-nimbus'].id
+    _zk = security_groups['teacup-storm-zk'].id
+
+    print('security_groups_zk: ["{}", "{}", "{}"]'.format(_default, _ssh, _zk))
+    print('security_groups_ni: ["{}", "{}", "{}"]'.format(_default, _ssh, _nimbus))
+    print('security_groups_sv: ["{}", "{}", "{}"]'.format(_default, _ssh, _sv))
+    print('security_groups_ui: ["{}", "{}", "{}"]'.format(_default, _ssh, _ui))
+    print(30*'-')
 
 def get_group_by_name (vpc, name):
+    """ Returns a SecurityGroup given its name. """
     sg = None
 
-    filters=[{"Name":"group-name", "Values": ["{}".format(name)]}]
-    for i in vpc.security_groups.filter(filters):
+    _filters=[{"Name":"group-name", "Values": ["{}".format(name)]}]
+    for i in vpc.security_groups.filter(Filters=_filters):
         sg = i
 
     return sg
@@ -69,9 +103,14 @@ def main():
     # Creates needed groups
     if security_groups['teacup-storm-zk'] == None:
         security_groups['teacup-storm-zk'] = create_zookeeper_sg(vpc)
-
-
-    # TODO other groups
+    if security_groups['teacup-storm-nimbus'] == None:
+        security_groups['teacup-storm-nimbus'] = create_nimbus_sg(vpc)
+    if security_groups['teacup-storm-sv'] == None:
+        security_groups['teacup-storm-sv'] = create_supervisor_sg(vpc)
+    if security_groups['teacup-storm-ssh'] == None:
+        security_groups['teacup-storm-ssh'] = create_ssh_sg(vpc)
+    if security_groups['teacup-storm-ui'] == None:
+        security_groups['teacup-storm-ui'] = create_ui_sg(vpc)
 
     # Prints configuration to paste into YAML file
     print_configuration()
